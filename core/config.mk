75d22504bd4eab7fd8532d4fc958b8a944a9bf21
Adnan Begovic <adnan@cyngn.com>
2015-05-26 18:21:15 -0700

    build/core: Inherit vendor/* makefiles for check-api.
    Change-Id: I72d409075192b18c35068566c5687092d45be9b4
    
    Conflicts:
    	core/config.mk

--- core/config.mk
+++ core/config.mk
@@ -623,4 +623,10 @@ $(eval include vendor/simpleaosp/sepolicy/sepolicy.mk)
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+# Include any vendor specific config.mk file
+-include $(TOPDIR)vendor/*/build/core/config.mk
+
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
